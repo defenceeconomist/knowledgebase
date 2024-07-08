d8f6d9f2b5cc1490864aa1e168de1bb90971a12e
lheley <lajh87@live.co.uk>
2024-07-08 07:46:53 +0100

    minor changes to style and structure

--- app.py
+++ app.py
@@ -6,7 +6,21 @@ import yaml
 with open("config.yaml", "r") as f:
     config = yaml.safe_load(f)
 
-app_ui = rag_ui("kb")
+page_dependencies = ui.tags.head(ui.include_css("./www/style.css"))
+
+app_ui = ui.page_navbar(   
+    ui.nav_control(page_dependencies),
+    ui.nav_panel(
+        "Retrieval Augmented Generation",
+        rag_ui("kb")
+    ),
+    ui.nav_spacer(),
+    ui.nav_control(ui.tags.a("The Defence Economist")),
+    title = "Knowledge Base",
+    fillable = True
+    )
+      
+   
 
 def server(input: Inputs, output: Outputs, session: Session):
    rag_server("kb", config)
--- ragui/ui.py
+++ ragui/ui.py
@@ -3,96 +3,61 @@ from ragui.icons import cogs, paper_plane
 
 from shiny import App, Inputs, Outputs, Session, module, render, ui
 
+page_sidebar = ui.sidebar(
+        ui.h4("About"),
+        ui.tags.span(
+            "This application uses ",
+            ui.input_action_link(
+                id = "info",
+                label = "Retrieval Augmented Generation"
+            ),
+            " to query an expanding knowledge base ",
+            "about Defence, Economics and Evaluation.",
+            style = "display: block-inline",
+            class_ = "help-block"
+            )
+        )
+    
+page_main = ui.TagList(
+    ui.card(
+        ui.output_ui("response"),
+        fill=True
+        ), 
+    
+    ui.card(
+        ui.row(
+            ui.column(
+                11,
+                    ui.input_text_area(
+                    "query", 
+                    None, 
+                    "What would you like to know about?", 
+                    width="100%",
+                    autoresize=True,
+                    resize="vertical"
+                ) 
+            ),
+            ui.column(
+                1,
+                ui.input_action_button(
+                    id ="ask", 
+                    label = None, 
+                    icon = paper_plane,
+                    width = "100%"),
+            )
+        ),
+        fill=False  
+        ),
+    ui.help_text("Chat GPT can make mistakes. Consider checking important information.",
+                    style = "text-align: center")
+)
 
 @module.ui
 def rag_ui():
+    return(ui.layout_sidebar(
+        page_sidebar,
+        page_main
+        ))
 
-    # Define UI ----
-    page_dependencies = ui.tags.head(
-        ui.include_css("./www/style.css")
-    )
     
-    page_sidebar = ui.sidebar(
-            ui.h4("About"),
-            ui.tags.span(
-                "This application uses ",
-                ui.input_action_link(
-                    id = "info",
-                    label = "Retrieval Augmented Generation"
-                ),
-                " to query an expanding knowledge base ",
-                "about Defence, Economics and Evaluation.",
-                style = "display: block-inline",
-                class_ = "help-block"
-                )
-            )
-    
-    page_main = ui.TagList(
-        ui.card(
-            ui.output_ui("response"),
-            fill=True
-            ), 
-       
-        ui.card(
-            ui.row(
-                ui.column(
-                    11,
-                     ui.input_text_area(
-                        "query", 
-                        None, 
-                        "What would you like to know about?", 
-                        width="100%",
-                        autoresize=True,
-                        resize="vertical"
-                    ) 
-                ),
-                ui.column(
-                    1,
-                    ui.input_action_button(
-                        id ="ask", 
-                        label = None, 
-                        icon = paper_plane,
-                        width = "100%"),
-                )
-            ),
-            fill=False  
-            ),
-        ui.help_text("Chat GPT can make mistakes. Consider checking important information.",
-                     style = "text-align: center")
-    )
-    
-    
-    app_ui = ui.page_navbar(
-        ui.nav_control(page_dependencies),
-        ui.nav_panel(
-            "Retrieval Augmented Generation",
-            ui.layout_sidebar(
-                page_sidebar,
-                page_main
-            )
-        ),
-        ui.nav_spacer(),
-        ui.nav_control(ui.tags.a("The Defence Economist")),
-         ui.nav_menu(
-            "Menu",
-            ui.nav_control(
-                ui.a(
-                    "Settings",
-                    href="https://shiny.posit.co/py/",
-                    target="_blank",
-                ),
-              ui.a(
-                    "Logout",
-                    href="https://posit.co",
-                    target="_blank",
-                )
-            ),
-            align="right",
-        ),
-        title = "Knowledge Base",
-        fillable = True
-        )
-      
-    return app_ui
-
 
