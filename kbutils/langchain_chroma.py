31e187f60df347dcab5bb5b42a15312f049e5ed7
defenceeconomist <defenceeconomist505@gmail.com>
2024-07-09 20:53:11 +0100

    added history, cache and store

--- kbutils/langchain_chroma.py
+++ kbutils/langchain_chroma.py
@@ -19,7 +19,7 @@ vector_db = connect_vectordb()
 
 # TODO 
 # [x] Contextual compression
-# [ ] Remembering previous question
+# [x] Remembering previous question
 # [ ] Citations
 # [ ] Streaming
 # [ ] Cache
@@ -37,7 +37,7 @@ def pretty_print_docs(docs):
 retriever = vector_db.as_retriever()
 query = 'What is realist evaluation?'
 docs = retriever.invoke(query)
-llm = OpenAI(temperature=0)
+llm = OpenAI(temperature=0.2)
 compressor = LLMChainExtractor.from_llm(llm)
 compression_retriever = ContextualCompressionRetriever(
     base_compressor=compressor, base_retriever=retriever
@@ -47,3 +47,139 @@ compressed_docs = compression_retriever.invoke(query)
 
 pretty_print_docs(docs)
 pretty_print_docs(compressed_docs)
+
+
+from langchain.retrievers.document_compressors import DocumentCompressorPipeline
+from langchain_community.document_transformers import EmbeddingsRedundantFilter
+from langchain_text_splitters import CharacterTextSplitter
+from langchain.retrievers.document_compressors import EmbeddingsFilter
+openai_ef = OpenAIEmbeddings(
+        model="text-embedding-ada-002"
+        )
+
+splitter = CharacterTextSplitter(chunk_size=300, chunk_overlap=0, separator=". ")
+redundant_filter = EmbeddingsRedundantFilter(embeddings=openai_ef )
+relevant_filter = EmbeddingsFilter(embeddings=openai_ef, similarity_threshold=0.76)
+pipeline_compressor = DocumentCompressorPipeline(
+    transformers=[splitter, redundant_filter, relevant_filter]
+)
+
+compression_retriever = ContextualCompressionRetriever(
+    base_compressor=pipeline_compressor, base_retriever=retriever
+)
+
+compressed_docs = compression_retriever.invoke(
+    "What is realist evaluation"
+)
+pretty_print_docs(compressed_docs)
+
+
+# Cache Results
+from langchain.globals import set_llm_cache
+from langchain.cache import InMemoryCache
+
+set_llm_cache(InMemoryCache())
+
+# History Aware QA
+from langchain.chains import create_history_aware_retriever
+from langchain_core.prompts import MessagesPlaceholder
+from langchain_core.prompts import ChatPromptTemplate
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain.chains import create_retrieval_chain
+
+contextualize_q_system_prompt = (
+    "Given a chat history and the latest user question "
+    "which might reference context in the chat history, "
+    "formulate a standalone question which can be understood "
+    "without the chat history. Do NOT answer the question, "
+    "just reformulate it if needed and otherwise return it as is."
+)
+
+contextualize_q_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", contextualize_q_system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+) 
+
+history_aware_retriever = create_history_aware_retriever(
+    llm, compression_retriever, contextualize_q_prompt
+)
+system_prompt = (
+    "You are an assistant for question-answering tasks. "
+    "Use the following pieces of retrieved context to answer "
+    "the question. If you don't know the answer, say that you "
+    "don't know. Use three sentences maximum and keep the "
+    "answer concise."
+    "\n\n"
+    "{context}"
+)
+
+
+
+qa_prompt = ChatPromptTemplate.from_messages(
+    [
+        ("system", system_prompt),
+        MessagesPlaceholder("chat_history"),
+        ("human", "{input}"),
+    ]
+)
+
+question_answer_chain = create_stuff_documents_chain(llm, qa_prompt)
+rag_chain = create_retrieval_chain(history_aware_retriever, question_answer_chain)
+
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.runnables.history import RunnableWithMessageHistory
+
+store = {}
+
+def get_session_history(session_id: str) -> BaseChatMessageHistory:
+    if session_id not in store:
+        store[session_id] = ChatMessageHistory()
+    return store[session_id]
+
+
+conversational_rag_chain = RunnableWithMessageHistory(
+    rag_chain,
+    get_session_history,
+    input_messages_key="input",
+    history_messages_key="chat_history",
+    output_messages_key="answer",
+)
+
+conversational_rag_chain.invoke(
+    {"input": "What is realistic evaluation?"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)["answer"]
+
+conversational_rag_chain.invoke(
+    {"input": "What is a common way of doing it?"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)["answer"]
+
+ans = conversational_rag_chain.invoke(
+    {"input": "Define CMO?"},
+    config={
+        "configurable": {"session_id": "abc123"}
+    },  # constructs a key "abc123" in `store`.
+)
+
+# View History
+from langchain_core.messages import AIMessage
+
+for message in store["abc123"].messages:
+    if isinstance(message, AIMessage):
+        prefix = "AI"
+    else:
+        prefix = "User"
+
+    print(f"{prefix}: {message.content}\n")
+
+
+store
