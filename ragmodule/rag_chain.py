2ab47ef89949116c9b9c1b6a7397c00ba7a388e7
lheley <lajh87@live.co.uk>
2024-07-19 13:05:09 +0100

    tidy up

--- ragmodule/rag_chain.py
+++ ragmodule/rag_chain.py
@@ -3,8 +3,6 @@ from langchain_core.prompts import MessagesPlaceholder
 from langchain_core.prompts import ChatPromptTemplate
 from langchain.chains.combine_documents import create_stuff_documents_chain
 from langchain.chains import create_retrieval_chain
-from langchain_community.chat_message_histories import ChatMessageHistory
-from langchain_core.chat_history import BaseChatMessageHistory
 from langchain_core.runnables.history import RunnableWithMessageHistory
 from langchain.globals import set_llm_cache
 
